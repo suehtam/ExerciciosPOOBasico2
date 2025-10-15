4686f69dbfcc4e56a05b08a80e6fcf35e2bf0f28
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-15 19:45:37 -0300

    Add program to print numbers from 1 to 50
    Implement a program to print numbers from 1 to 50 using a for loop.

---
+++ Exercicio3.java
@@ -0,0 +1,16 @@
+/*
+3. Números de 1 a 50 com for
+Desenvolva um programa que imprima os números de 1 a 50, um por linha, usando um
+loop for tradicional.
+*/
+import java.util.Scanner;
+class Exercicio3
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <= 50; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
