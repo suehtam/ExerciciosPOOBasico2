15e018322991d93491acf27373f1a6df02a2775d
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-10-16 16:21:57 -0300

    Add countdown program in Exercicio7.java
    Implement a countdown program that prints numbers from 10 to 1.

---
+++ Exercicio7.java
@@ -0,0 +1,15 @@
+/*
+7. Contagem Regressiva 
+Desenvolva um programa que utilize um loop for para imprimir os números de 10 a 1 
+em ordem decrescente. 
+*/
+class Exercicio7
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 10; i >= 1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
